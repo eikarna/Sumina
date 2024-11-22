e420ef5e42c05e90bb15923925f692ce305be120
Eikarna <nixic0@proton.me>
2024-11-22 20:58:51 +0700

    Update index.js

--- server.js
+++ server.js
@@ -14,7 +14,11 @@ require("dotenv").config();
 const apii = require(path.join(__dirname, "./api"));
 
 // Load API Docs
-app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+const CSS_URL = "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.19.1/swagger-ui.css";
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec, {
+  customCss: '.swagger-ui .opblock .opblock-summary-path-description-wrapper { align-items: center; display: flex; flex-wrap: wrap; gap: 0 10px; padding: 0 10px; width: 100%; }',
+    customCssUrl: CSS_URL,
+}));
 
 // Apply Rate limits
 // app.use(rateLimit);
